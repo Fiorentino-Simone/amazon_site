574967830d8e15f4fe5a9447a061e6785df04cc0
Fiorentino-Simone <s.fiorentino.1743@vallauri.edu>
2022-05-05 20:41:26 +0200

    Complete second navbar

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 "use strict";
 
 $(document).ready(function(){
-    let button = `.buttonHeader, 
+    let buttons = `.buttonHeader, 
     .buttonAccedi, 
     .buttonOrdini,
     .buttonCarrello,
-    .buttonAll`;
-    $(button).on("mouseenter",function(){
+    .buttonAll,
+    .buttonBestSeller div,
+    .buttonAmazonBasic div,
+    .buttonClienti div,
+    .buttonOfferte div,
+    .buttonNovita div,
+    .buttonPrime div,
+    .buttonMusic div,
+    .buttonModa div,
+    .buttonLibri div,
+    .buttonVideogiochi div,
+    .buttonElettronica div,
+    .buttonCucina div`;
+
+    $(buttons).on("mouseenter",function(){
         $(this).css("border","1px solid white");
+        if($(this).hasClass("libri")) $(this).css("left","510%");
     });
-    $(button).on("mouseleave",function(){
+
+    $(buttons).on("mouseleave",function(){
         $(this).css("border","");
+        if($(this).hasClass("libri")) $(this).css("left","");
     });
 
     $("#dropdownMenuButton1").on("click",
     function(){
         $(this).show();
     });
-
-    /*
-    .buttonBestSeller,
-    .buttonAmazonBasic,
-    .buttonClienti,
-    .buttonOfferte,
-    .buttonNovita,
-    .buttonPrime,
-    .buttonMusic,
-    .buttonModa,
-    .buttonLibri,
-    .buttonVideogiochi,
-    .buttonElettronica,
-    .buttonCucina
-
-    DA CORRREGGERE IL BORDER*/
 });
